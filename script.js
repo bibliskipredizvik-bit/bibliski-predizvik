a08b5135283011e23019c8c94075b581259b83e6
bibliskipredizvik-bit <bibliski.predizvik@gmail.com>
2026-07-13 08:23:58 +0200

    Update script.js version 1.1

--- script.js
+++ script.js
@@ -1,12 +1,117 @@
+const questions = [
+
+{
+question: "Кој ја изградил Ноевата арка?",
+answers: [
+"Авраам",
+"Ное",
+"Мојсеј"
+],
+correct: 1
+},
+
+{
+question: "Колку ученици имал Исус Христос?",
+answers: [
+"10",
+"12",
+"20"
+],
+correct: 1
+},
+
+{
+question: "Кој го победил џинот Голијат?",
+answers: [
+"Давид",
+"Соломон",
+"Петар"
+],
+correct: 0
+},
+
+{
+question: "Кој бил првиот човек според Библијата?",
+answers: [
+"Адам",
+"Мојсеј",
+"Павле"
+],
+correct: 0
+},
+
+{
+question: "Кој го предал Исус?",
+answers: [
+"Јован",
+"Јуда",
+"Лука"
+],
+correct: 1
+},
+
+{
+question: "Кој го разделил Црвеното Море?",
+answers: [
+"Мојсеј",
+"Давид",
+"Ное"
+],
+correct: 0
+},
+
+{
+question: "Кој бил таткото на Исак?",
+answers: [
+"Авраам",
+"Јаков",
+"Јосиф"
+],
+correct: 0
+},
+
+{
+question: "Кој бил првиот цар на Израел?",
+answers: [
+"Давид",
+"Саул",
+"Соломон"
+],
+correct: 1
+},
+
+{
+question: "Кој го напишал најголемиот број Псалми?",
+answers: [
+"Давид",
+"Петар",
+"Матеј"
+],
+correct: 0
+},
+
+{
+question: "Во кој град бил роден Исус?",
+answers: [
+"Назарет",
+"Витлеем",
+"Ерусалим"
+],
+correct: 1
+}
+
+];
+
 
 
 let currentQuestion = 0;
 let score = 0;
 
 
+
 function loadQuestion(){
 
-let q = questions[currentQuestion];
+const q = questions[currentQuestion];
 
 
 document.querySelector(".quiz-box h2").innerHTML =
@@ -44,21 +149,24 @@ document.querySelector('input[name="answer"]:checked');
 
 if(!selected){
 
-alert("Ве молиме изберете одговор.");
+alert("Изберете одговор пред да продолжите.");
 
 return;
 
 }
 
 
-if(parseInt(selected.value) === questions[currentQuestion].correct){
+
+if(Number(selected.value) === questions[currentQuestion].correct){
 
 score++;
 
 }
 
 
+
 currentQuestion++;
+
 
 
 if(currentQuestion < questions.length){
@@ -70,35 +178,28 @@ loadQuestion();
 else{
 
 
-let playerName = localStorage.getItem("name") || "Играч";
+document.querySelector(".quiz-box").innerHTML =
 
+`
+<h2>Квизот е завршен!</h2>
 
-let playerSurname = localStorage.getItem("surname") || "";
-
-
-document.querySelector(".quiz-box").innerHTML = `
-
-<h2>Квизот е завршен! 🏆</h2>
-
-
-<p>
-Браво ${playerName} ${playerSurname}!
-</p>
-
+<h3>
+Ваш резултат:
+<br><br>
+${score} / ${questions.length}
+</h3>
 
 <p>
-Ваш резултат е:
-<strong>${score} / ${questions.length}</strong>
+${Math.round((score / questions.length) * 100)}%
 </p>
 
-
-<button onclick="location.href='index.html'">
-Почетна
-</button>
-
+<p>
+Продолжете со проучување на Светото Писмо.
+</p>
 `;
 
 }
+
 
 
 }
